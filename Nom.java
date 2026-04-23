f5b83a275e2560bfc5084be8624470f3bdb4a842
omarhmida <omar.hmida@etudiant-enit.utm.tn>
2026-04-23 10:42:04 +0100

    l'ajout de set et get

--- Nom.java
+++ Nom.java
@@ -2,9 +2,21 @@ public class Nom {
     private String name;
     private String id;
     private String nomPretraite;
+
+    
     public Nom(String name, String id) {
         this.name = name;
         this.id = id;
     }
-    
+
+    public String getName() {
+        return name;
+    }
+    public void setNomPretraite(String nomPretraite) {
+        this.nomPretraite = nomPretraite;
+    }
+    public String getNomPretraite() {
+        return nomPretraite;
+    }   
 }
+
