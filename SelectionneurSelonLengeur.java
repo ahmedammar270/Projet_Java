4ecaf02e55ad0d4cc0caac65839490cdd5316104
omarhmida <omar.hmida@etudiant-enit.utm.tn>
2026-05-06 18:13:02 +0100

    selectionneur selon la taille du fichier

---
+++ SelectionneurSelonLengeur.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SelectionneurSelonLengeur extends SelectionneurDeCanditat {
+    public List<String> selectionner(String fichier,Nom nom){
+        String nomOriginale = nom.getName();
+        int longueur = nomOriginale.length();
+        List<String> resultats = new ArrayList<>();
+
+        
+        double tolerance = 0.3; // 30%
+
+        try (BufferedReader br = new BufferedReader(new FileReader(fichier))) {
+
+            String ligne;
+
+            while ((ligne = br.readLine()) != null) {
+
+                String[] v = ligne.split(",");
+                if (v.length < 2) continue;
+
+                String name = v[1];
+
+                int lenNom = name.length();
+
+                // 🔥 condition
+                if (Math.abs(lenNom - longueur) <= tolerance * longueur || Math.abs(lenNom - longueur) >= tolerance * longueur) {
+                    resultats.add(name);
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return resultats;
+    }
+}
+
+
